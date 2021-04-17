0e40361cf8f2dd46804a582535145e247331c898
Enrique Collazo <ecollazo1991@gmail.com>
2021-04-16 21:01:57 -0400

    all routes are functional

--- server.js
+++ server.js
@@ -2,11 +2,12 @@ const path = require("path");
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
-const Workout = require("./models/workout.js")
+const router = require('./routes');
 
 const PORT = process.env.PORT || 3000;
 
-const db = require("./models");
+// const Workout = require("./models/workout.js")
+// const db = require("./models");
 
 const app = express();
 
@@ -14,41 +15,16 @@ app.use(logger("dev"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(express.static("public"));
-
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
-
-app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'))
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+    useNewUrlParser: true,
+    useFindAndModify: false
 });
 
-app.get("/exercise", (req, res) => {
-    res.sendFile(path.join(__dirname, './public/exercise.html'))
-});
-
-app.get("/stats", (req, res) => {
-    res.sendFile(path.join(__dirname, './public/stats.html'))
-});
-
-//
-
-app.get("/api/workouts", (req, res) => {
-    Workout.aggregate([{
-        $addFields: {
-            totalDuration: {
-                $sum: "$exercises.duration"
-            }
-        }
-    }]).then(allWorkouts => {
-        console.log(allWorkouts);
-        res.json(allWorkouts);
-    }).catch(err => {
-        res.json(err);
-    });
-})
+app.use(router);
 
 // Start the server
 app.listen(PORT, () => {
     console.log(`App running on port ${PORT}!`);
-  });
+});
